891af9e24c7ca671b6ff64b6b63ae8286d3a1858
jossduff <jduff360@gmail.com>
2023-12-01 15:24:52 +0200

    async fn example and skar crates

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,19 @@ fn send_req() -> PyResult<String> {
     Ok("Sent a super cool HyperSync skar query".to_string())
 }
 
+#[pyfunction]
+fn rust_sleep(py: Python) -> PyResult<&PyAny> {
+    pyo3_asyncio::tokio::future_into_py(py, async {
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        Ok(())
+    })
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn hypersync_client(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(send_req, m)?)?;
+    m.add_function(wrap_pyfunction!(rust_sleep, m)?)?;
     Ok(())
 }
