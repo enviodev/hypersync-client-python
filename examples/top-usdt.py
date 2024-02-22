56ebd1c40234be1440d9a35944cd1871b67f3407
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-02-22 12:25:11 +0600

    update top-usdt example

--- examples/top-usdt.py
+++ examples/top-usdt.py
@@ -1,5 +1,5 @@
 import hypersync
-from hypersync import LogSelection, LogField, DataType, FieldSelection, ColumnMapping
+from hypersync import LogSelection, LogField, DataType, FieldSelection, ColumnMapping, TransactionField
 import asyncio
 import polars
 
@@ -16,9 +16,15 @@ async def collect_events():
         )],
         field_selection=FieldSelection(
             log=[
+                LogField.TOPIC0,
                 LogField.TOPIC1,
                 LogField.TOPIC2,
                 LogField.DATA,
+                LogField.TRANSACTION_HASH,
+            ],
+            transaction=[
+                TransactionField.HASH,
+                TransactionField.GAS_USED,
             ]
         ),
     )
@@ -29,7 +35,10 @@ async def collect_events():
         column_mapping=ColumnMapping(
             decoded_log={
                 "value": DataType.FLOAT64,
-            }
+            },
+            transaction={
+                TransactionField.GAS_USED: DataType.FLOAT64,
+            },
         ),
         event_signature="Transfer(address indexed from, address indexed to, uint256 value)",
     )
@@ -37,14 +46,25 @@ async def collect_events():
     await client.create_parquet_folder(query, config)
 
 def analyze_events():
+    logs = polars.read_parquet(
+        "data/logs.parquet",
+    )
+
+    transactions = polars.read_parquet(
+        "data/transactions.parquet",
+    )
+    
     data = polars.read_parquet(
         "data/decoded_logs.parquet"
-    ).select(
-        polars.col("*")
+    ).hstack(logs).join(
+        other=transactions,
+        left_on=polars.col("transaction_hash"),
+        right_on=polars.col("hash"),
     ).group_by(
         polars.col("from")
     ).agg(
-        polars.col("value").sum().alias("total_value_sent")
+        polars.col("value").sum().alias("total_value_sent"),
+        polars.col("gas_used").sum().alias("total_gas_used"),
     ).sort(
         polars.col("total_value_sent"),
         descending=True
